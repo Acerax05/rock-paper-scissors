099c34938fac068e20a538f1f3fdfeab4986b163
Anthony <159395736+Acerax05@users.noreply.github.com>
2024-08-26 23:38:08 -0400

    Add function to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+function getComputerChoice() {
+    getNumber = Math.floor(Math.random()* 3);
+    console.log(getNumber)
+    if (getNumber === 1) {
+        return "Rock"
+    }
+    else if (getNumber === 2) {
+        return "Paper"
+    }
+    else {
+        return "Scissors"
+    }
+
+
+}
+
+console.log(getComputerChoice())
